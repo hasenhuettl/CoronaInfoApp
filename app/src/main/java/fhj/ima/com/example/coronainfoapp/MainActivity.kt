1f1933e07382a37857d0b51eb9690bf3ba7983b5
verena79 <vbrettschuh@gmail.com>
2020-12-29 16:52:01 +0100

    onClickListener

--- app/src/main/java/fhj/ima/com/example/coronainfoapp/MainActivity.kt
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package at.fh.swengb.coronainfoapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fhj.ima.com.example.coronainfoapp.Beschraenkungen
+import fhj.ima.com.example.coronainfoapp.aktuelleZahlen
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        call_button.setOnClickListener{
+            val call = Intent(Intent.ACTION_DIAL)
+            call.setData(Uri.parse("tel:" +"1450"))
+            startActivity(call)
+        }
+
+        aktuelleZahlen_button.setOnClickListener {
+            val intent = Intent(this, aktuelleZahlen::class.java)
+            startActivity(intent)
+        }
+
+        beschränkungen_button.setOnClickListener {
+            val intent = Intent(this, Beschraenkungen::class.java)
+            startActivity(intent)
+        }
+
+        open_erklärung.setOnClickListener {
+            val intent = Intent(this, Ampelfarben::class.java)
+            startActivity(intent)
+        }
     }
 }
