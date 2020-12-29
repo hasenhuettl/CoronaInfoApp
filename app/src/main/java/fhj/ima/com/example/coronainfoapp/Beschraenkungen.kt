5b02130b3cff90c70aaa49cf432855ad1426ca1d
verena79 <vbrettschuh@gmail.com>
2020-12-29 15:54:38 +0100

    Beschraenkungen

--- app/src/main/java/fhj/ima/com/example/coronainfoapp/Beschraenkungen.kt
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/Beschraenkungen.kt
@@ -1,11 +1,12 @@
-package at.fh.swengb.coronainfoapp
+package fhj.ima.com.example.coronainfoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import at.fh.swengb.coronainfoapp.R
 
-class MainActivity : AppCompatActivity() {
+class Beschraenkungen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_beschraenkungen)
     }
 }
