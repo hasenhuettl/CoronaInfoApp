3e50f5c5bfb5d2a30b8d291aa2110529d8559bf6
verena79 <vbrettschuh@gmail.com>
2020-12-29 15:16:37 +0100

    Test

---
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/Ampelfarben.kt
@@ -0,0 +1,11 @@
+package at.fh.swengb.coronainfoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ampelfarben : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ampelfarben)
+    }
+}
