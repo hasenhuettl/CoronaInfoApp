d346c1e1152c05bc43b549e1cfe0fc5e57b1422c
David <hasi1999@gmx.at>
2020-12-29 17:01:49 +0100

    strings

--- app/src/main/java/fhj/ima/com/example/coronainfoapp/AktuelleZahlen.kt
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/AktuelleZahlen.kt
@@ -1,11 +1,14 @@
 package fhj.ima.com.example.coronainfoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import at.fh.swengb.coronainfoapp.R
 
-class aktuelleZahlen : AppCompatActivity() {
+class AktuelleZahlen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aktuelle_zahlen)
+
     }
 }
