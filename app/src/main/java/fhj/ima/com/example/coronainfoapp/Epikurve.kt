3fc0fd33b065e2d83f5c82c44073af24f97c82d3
David <hasi1999@gmx.at>
2021-01-15 12:14:43 +0100

    Nightmode and Persistence

--- app/src/main/java/fhj/ima/com/example/coronainfoapp/Epikurve.kt
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/Epikurve.kt
@@ -1,23 +1,16 @@
 package fhj.ima.com.example.coronainfoapp
 
-import android.app.Activity
-import android.app.DownloadManager
-import android.app.Instrumentation
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Environment
-import android.view.View
+import android.util.TypedValue
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import at.fh.swengb.coronainfoapp.R
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -26,22 +19,18 @@ import kotlinx.android.synthetic.main.activity_ampelfarben.*
 import kotlinx.android.synthetic.main.activity_epikurve.*
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
-import java.io.IOException
 import java.util.*
 import kotlin.collections.ArrayList
-import java.util.Timer
-import kotlin.concurrent.schedule
-import kotlin.concurrent.thread
 
 
 class AktuelleZahlen : AppCompatActivity() {
     val epikurvenAdapter = EpikurveAdapter()
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_epikurve)
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("LocalPersistence", Context.MODE_PRIVATE)
 
         EpikurveRepository.epikurveList(
                 success = {
@@ -56,20 +45,21 @@ class AktuelleZahlen : AppCompatActivity() {
         epikurve_recycler_view.layoutManager = LinearLayoutManager(this)
         epikurve_recycler_view.adapter = epikurvenAdapter
 
-        epikurve_button.setOnClickListener{
-            diagram() // ToDO einstellen der angezeigten Tage
-        }
 
         // Create an ArrayAdapter
-        val adapter = ArrayAdapter.createFromResource(this,
-            R.array.city_list, android.R.layout.simple_spinner_item)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.spinner_numbers, android.R.layout.simple_spinner_item)
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         // Apply the adapter to the spinner
         spinner.adapter = adapter
 
 
-     
+        spinner.setSelection(sharedPreferences.getInt("MY_KEY_FOR_SPINNERSELECTION", 0))
+
+        epikurve_button.setOnClickListener{
+            diagram()
+            sharedPreferences.edit().putInt("MY_KEY_FOR_SPINNERSELECTION", adapter.getPosition(spinner.selectedItem.toString())).apply()
+        }
     }
 
 
@@ -77,7 +67,7 @@ class AktuelleZahlen : AppCompatActivity() {
 
 
 
-fun diagram(){ // ToDO skalierung und graphenbeschriftungen
+fun diagram(){
     //Part1
     val entries = ArrayList<Entry>()
     var iteration = 0
@@ -88,7 +78,7 @@ fun diagram(){ // ToDO skalierung und graphenbeschriftungen
     var x = 0
     var y = 0
     while ( iteration > x){
-        val wert = epikurvenAdapter.getNewValue(iteration-1)
+        val wert = epikurvenAdapter.getNewValue(iteration - 1)
         entries.add(Entry(y.toFloat(), wert.toFloat()))
         iteration--
         y++
@@ -98,11 +88,21 @@ fun diagram(){ // ToDO skalierung und graphenbeschriftungen
     val vl = LineDataSet(entries, "Anzahl der Fälle")
 
     //Part4
+    val typedValue = TypedValue()
+    theme.resolveAttribute(R.attr.colorOnPrimary, typedValue, true)
+    val mycolor = typedValue.data
+
+
     vl.setDrawValues(false)
     vl.setDrawFilled(true)
     vl.lineWidth = 3f
 //    vl.fillColor = R.color.light_teal
-    vl.fillAlpha = R.color.light_teal
+    vl.fillAlpha = mycolor
+    lineChart.axisLeft.textColor = mycolor
+    lineChart.axisRight.textColor = mycolor
+    lineChart.xAxis.textColor = mycolor
+    lineChart.legend.textColor = mycolor
+    lineChart.description.textColor = mycolor
 
     //Part5
     lineChart.xAxis.labelRotationAngle = 0f
@@ -113,6 +113,8 @@ fun diagram(){ // ToDO skalierung und graphenbeschriftungen
     //Part7
     //lineChart.axisRight.isEnabled = false
     //lineChart.xAxis.axisMaximum = j+0.1f
+    val yAxis: YAxis = lineChart.axisLeft
+    yAxis.setAxisMinimum(0f);
 
 
     //Part8
--- app/src/main/java/fhj/ima/com/example/coronainfoapp/MainActivity.kt
+++ app/src/main/java/fhj/ima/com/example/coronainfoapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package at.fh.swengb.coronainfoapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import fhj.ima.com.example.coronainfoapp.Beschraenkungen
 import fhj.ima.com.example.coronainfoapp.AktuelleZahlen
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +15,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("LocalPersistence", Context.MODE_PRIVATE)
+        sharedPreferences.getBoolean("KEY_DARKMODE", false).also { main_switch_darkmode.isChecked = it }
+        if (main_switch_darkmode.isChecked) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
 
         call_button.setOnClickListener{
             val call = Intent(Intent.ACTION_DIAL)
@@ -33,5 +46,17 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Ampelfarben::class.java)
             startActivity(intent)
         }
+
+        main_switch_darkmode.setOnClickListener{
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putBoolean("KEY_DARKMODE", main_switch_darkmode.isChecked)
+            editor.apply()
+            if (main_switch_darkmode.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
+
 }
